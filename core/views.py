df853adefd814adfd7d6eda9bfa6984b53f2e8fd
asiannelson <nelson14h@yahoo.com>
2018-03-29 21:33:43 -0600

    nhsiao-hw7

--- core/views.py
+++ core/views.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import Context, loader
+
 # Create your views here.
+def index(request):
+    template = loader.get_template('index.html') 
+    return HttpResponse(template.render())
