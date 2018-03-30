6f55bec87064775ed00b88ff301d19b74331887c
asiannelson <nelson14h@yahoo.com>
2018-03-29 21:38:59 -0600

    nhsiao-hw7

--- core/views.py
+++ core/views.py
@@ -8,5 +8,5 @@ from django.template import Context, loader
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html') 
+    template = loader.get_template('home.html') 
     return HttpResponse(template.render())
